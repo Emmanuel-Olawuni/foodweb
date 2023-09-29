41e6f8df0216cb993b25f444c2e71f7f79383e8f
Emmanuel <emmanuelolawuni2001@gmail.com>
2023-09-29 08:34:48 +0100

    final version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,10 @@ import HeroFood from "./AnimatedImages/heroFood";
 
 const Hero = () => {
   return (
-    <div className="hero_container h-[90vh] md:h-[70vh] flex  items-center md:z-30 relative mt-[3rem] md:mt-0 hero_container">
+    <div className="hero_container h-[80vh] md:h-[70vh] flex justify-center md:justify-start  items-center md:z-30 relative mt-[4rem] md:mt-0 ">
       <div className=" flex flex-around flex-col md:flex-row items-center justify-center">
         <div className=" flex flex-col gap-5">
-          <p className=" font-miniver text-3xl md:text-lg text-green  font-normal">
+          <p className=" font-miniver text-2xl md:text-lg text-green  font-normal">
             Healthy & Tasty Food
           </p>
           <h3 className=" font-inter text-6xl leading-[70px] font-extrabold text-gray-800 text-left balance">
